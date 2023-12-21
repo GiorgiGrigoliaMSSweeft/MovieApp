3f46364881e26349ce012e16a5c77bb061498a42
Giorgi Grigolia <giorgi.grigolia@makingscience.com>
2023-12-21 10:27:33 +0400

    🔗 Implemented mapping to append the base URL prefix to poster path URLs for obtaining the full image URLs.

--- app/src/main/java/com/example/movieapp/AppViewModel.kt
+++ app/src/main/java/com/example/movieapp/AppViewModel.kt
@@ -5,10 +5,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.movieapp.network.NetworkModule.TrendingTodayApi
-import com.example.movieapp.network.NetworkModule.LatestSeriesApi
-import com.example.movieapp.network.model.LatestMoviesItem
 import com.example.movieapp.network.NetworkModule.LatestMoviesApi
+import com.example.movieapp.network.NetworkModule.LatestSeriesApi
+import com.example.movieapp.network.NetworkModule.TrendingTodayApi
+import com.example.movieapp.network.model.LatestMoviesItem
 import com.example.movieapp.network.model.LatestSeriesItem
 import com.example.movieapp.network.model.TrendingTodayItem
 import com.example.movieapp.network.statuses.LatestMoviesApiStatus
@@ -43,15 +43,20 @@ class AppViewModel : ViewModel() {
     val retrievedTrendingTodayItems: LiveData<List<TrendingTodayItem>> =
         _retrievedTrendingTodayItems
 
-
     fun getLatestMovies() {
         viewModelScope.launch {
             _latestMoviesStatus.value = LatestMoviesApiStatus.LOADING
             try {
                 val latestMovies = LatestMoviesApi.retrofitService.getLatestMovies()
-                _retrievedLatestMovies.value = latestMovies.results
+
+                // Modifies the poster path to include the base URL
+                val moviesWithFullPosterPath = latestMovies.results.map { movie ->
+                    movie.copy(posterPath = POSTER_BASE_URL + movie.posterPath)
+                }
+
+                _retrievedLatestMovies.value = moviesWithFullPosterPath
                 _latestMoviesStatus.value = LatestMoviesApiStatus.DONE
-                Log.d("Tag", "Successfully retrieved latest movies")
+                Log.d("Tag", retrievedLatestMovies.value.toString())
             } catch (e: Exception) {
                 _latestMoviesStatus.value = LatestMoviesApiStatus.ERROR
                 _retrievedLatestMovies.value = listOf()
@@ -59,14 +64,21 @@ class AppViewModel : ViewModel() {
         }
     }
 
+
     fun getLatestSeries() {
         viewModelScope.launch {
             _latestSeriesStatus.value = LatestSeriesApiStatus.LOADING
             try {
                 val latestSeries = LatestSeriesApi.retrofitService.getLatestSeries()
-                _retrievedLatestSeries.value = latestSeries.results
+
+                // Modifies the poster path to include the base URL
+                val seriesWithFullPosterPath = latestSeries.results.map { series ->
+                    series.copy(posterPath = POSTER_BASE_URL + series.posterPath)
+                }
+
+                _retrievedLatestSeries.value = seriesWithFullPosterPath
                 _latestSeriesStatus.value = LatestSeriesApiStatus.DONE
-                Log.d("Tag", "Successfully retrieved latest series")
+                Log.d("Tag", retrievedLatestSeries.value.toString())
             } catch (e: Exception) {
                 _latestSeriesStatus.value = LatestSeriesApiStatus.ERROR
                 _retrievedLatestSeries.value = listOf()
@@ -79,13 +91,23 @@ class AppViewModel : ViewModel() {
             _trendingTodayStatus.value = TrendingTodayApiStatus.LOADING
             try {
                 val trendingTodayItems = TrendingTodayApi.retrofitService.getLatestTrendingItems()
-                _retrievedTrendingTodayItems.value = trendingTodayItems.results
+
+                // Modifies the poster path to include the base url
+                val trendingTodayItemsWithFullPosterPath = trendingTodayItems.results.map { trendingTodayItem ->
+                    trendingTodayItem.copy(posterPath = POSTER_BASE_URL + trendingTodayItem.posterPath)
+                }
+
+                _retrievedTrendingTodayItems.value = trendingTodayItemsWithFullPosterPath
                 _trendingTodayStatus.value = TrendingTodayApiStatus.DONE
-                Log.d("Tag", "Successfully retrieved todays trending items")
+                Log.d("Tag", retrievedTrendingTodayItems.value.toString())
             } catch (e: Exception) {
                 _trendingTodayStatus.value = TrendingTodayApiStatus.ERROR
                 _retrievedTrendingTodayItems.value = listOf()
             }
         }
+    }
+
+    companion object {
+        private const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w500"
     }
 }
